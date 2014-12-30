64b147f9b9f3b205432388e48ff63514e961424a
ruichen <rayprogram@163.com>
2014-12-30 13:09:28 +0800

    添加两种新的查询

--- application/web/model/pic_posi_bias.py
+++ application/web/model/pic_posi_bias.py
@@ -1,49 +1,53 @@
-#coding:utf-8
+#coding: utf-8
 __author__ = 'CRay'
 
-import codecs
 import operator
+import sys
+sys.path.append(r'../../')
 from random import choice
 from ConfigParser import ConfigParser
 
 
 class PositionBias(object):
-    def __init__(self, min_show, min_page, max_pic_num):
+    def __init__(self, min_show=None, min_page=None, max_pic_num=None):
         self.min_show = min_show
         self.min_page = min_page
         self.max_pic_num = max_pic_num
-        self.data = PositionBias.filter_data(self.min_show, self.min_page, self.max_pic_num)
 
     @staticmethod
-    def filter_data(min_show, min_page, max_pic_num):
+    def get_file_path(name):
         cf = ConfigParser()
         cf.read('../../config/data.conf')
-        file_path = cf.get('ray', 'path')
+        file_path = cf.get('ray', 'path') + name
+        return file_path
+
+    def get_specific_pic(self):
+        file_path = PositionBias.get_file_path('pic_position_hour')
         dict_output = {}
         limit_pic = set()
-        fin = codecs.open(file_path, 'r', encoding='utf-8')
+        fin = open(file_path, 'r')
         dict_raw = eval(fin.readline())
-        for picture in dict_raw:
+        while len(limit_pic) < self.max_pic_num:
+            random_pic = choice(dict_raw.keys())
             count = 0
             temp = {}
-            for page in dict_raw[picture]:
-                if dict_raw[picture][page][0] > min_show:
+            for page in dict_raw[random_pic]:
+                if dict_raw[random_pic][page][0] > self.min_show:
                     count += 1
-                    temp[page] = dict_raw[picture][page]
-            if count < min_page:
+                    temp[page] = dict_raw[random_pic][page]
+            if count < self.min_page:
                 continue
-            dict_output[picture] = temp
-        fin.close()
-        while len(limit_pic) < max_pic_num:
-            random_pic = choice(dict_output.keys())
+            dict_output[random_pic] = temp
             limit_pic.add(random_pic)
-        list_data = PositionBias.print_data(dict_output, limit_pic)
+        fin.close()
+        list_data = PositionBias.print_pb(dict_output)
+        print '随机抽取的几张图片: ', list_data
         return list_data
 
     @staticmethod
-    def print_data(result, max_pic_num):
+    def print_pb(result):
         list_output = []
-        for picture in max_pic_num:
+        for picture in result:
             dict_per_pic = {}
             temp = result[picture]
             sorted_tuple = sorted(temp.items(), key=lambda d: d[0], reverse=True)
@@ -63,5 +67,43 @@ class PositionBias(object):
         return list_output
 
     @staticmethod
-    def get_pic_pb(min_show, min_page, max_pic_num):
-        return PositionBias(min_show, min_page, max_pic_num)
+    def get_pb_hour():
+        file_path = PositionBias.get_file_path('pic_hour_distribution')
+        fin = open(file_path, 'r')
+        list_raw = eval(fin.readline())
+        fin.close()
+        print '点击概率与时刻的关系: ', list_raw
+        return list_raw
+
+    @staticmethod
+    def merge_pics_into_one_line(min_show, min_page):
+        file_path = PositionBias.get_file_path('pic_position_hour')
+        dict_output = {}
+        fin = open(file_path, 'r')
+        dict_raw = eval(fin.readline())
+        for picture in dict_raw:
+            count = 0
+            temp = {}
+            for page in dict_raw[picture]:
+                if dict_raw[picture][page][0] > min_show:
+                    count += 1
+                    temp[page] = dict_raw[picture][page]
+            if count < min_page:
+                continue
+            for page in temp:
+                if page not in dict_output:
+                    dict_output[page] = [0, 0]
+                dict_output[page][0] += temp[page][0]
+                dict_output[page][1] += temp[page][1]
+        fin.close()
+        list_output = []
+        for i in range(1, 100):
+            if i not in dict_output:
+                continue
+            temp = float(dict_output[i][1])/dict_output[i][0]
+            list_output.append([i, round(temp, 3)])
+        print '结果集合汇成一条线: ', list_output
+        return list_output
+
+if __name__ == '__main__':
+    PositionBias.get_pb_hour()
