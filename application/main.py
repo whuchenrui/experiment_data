8a4ebbb4c0f66d5b083e996c87aa5df9aa92c5be
ruichen <rayprogram@163.com>
2014-12-30 13:13:02 +0800

    简单更新

--- application/main.py
+++ application/main.py
@@ -14,8 +14,8 @@ from application.turn.turn_probability import *
 def link_files():
     """ 整合每小时产生的序列，合并为一个文件，未过滤。返回序列总长度，
     并找出大于K次请求后，只剩5%的用户时候的请求数值，方便下一轮过滤 """
-    folder_st = '2014-10-26'
-    folder_end = '2014-10-31'
+    folder_st = '2014-11-04'
+    folder_end = '2014-11-24'
     line_cnt, filter_req = link(folder_st, folder_end)
     print '产生记录： ' + str(line_cnt) + '条'
     print '98% is: ' + str(filter_req)
@@ -37,10 +37,10 @@ def main_filter_data(_filter_request, _type):
 
 def pic_click(_filter_req, _type):
     """ 画平均点击图和点图 """
-    distribution(_type)                       # 用户序列分布图
+    # distribution(_type)                       # 用户序列分布图
     # average_click(_filter_req, _action)     # 平均点击图
     # quality(_action)                        # 点图，证明图片质量差别
-    # position_bais(_filter_req, _action)     # page的postion图
+    position_bias(_filter_req, _type)     # page的postion图
     # get_pic_click(_type, 25, 4)             # 单张图片的position图
     # find_pic(12, )
 
@@ -53,16 +53,13 @@ def turn(_action):
 if __name__ == '__main__':
     # TODO:修改view的时候，一定要顺带修改data.conf 中的type为相应值！！！！
     act_type = 'view'
-    link_hour('2014-10-26', '2014-10-31', 25, 5)
+    # link_hour('2014-11-04', '2014-11-23', 25, 8)
     # filter_request = link_files()
     # filter_request = 28
     # main_filter_data(filter_request, act_type)
-    # pic_click(filter_request, act_type)
+    pic_click(29, act_type)
     # turn(action)
-    # turn_probability(act_type, 2, 15, 20, 2, 0)
-    # turn_probability(act_type, 3, 15, 20, 3, 2)
     # turn_probability(act_type, 4, 15, 20, 4, 3)
 
-    # 找图片
     # get_pic_click(act_type, 25, 6)
     # find_pic(6, 500, 100)
