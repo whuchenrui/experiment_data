ae21198c4a9d515da9edae04994cbf0120dd7e76
ruichen <rayprogram@163.com>
2015-01-09 16:07:13 +0800

    映射relate_pic的处理handler

--- application/web/urls.py
+++ application/web/urls.py
@@ -3,10 +3,11 @@ __author__ = 'CRay'
 
 from handlers.PosBaisHandler import MainHandler
 from handlers.PosBaisHandler import PicHourHandler
+from handlers.RelatePicHandler import RelatePicHandler
 
 
 urls = [
     (r'/pb', MainHandler),
     (r'/hour', PicHourHandler),
-    (r'/page', PicHourHandler),
+    (r'/page', RelatePicHandler),
 ]
