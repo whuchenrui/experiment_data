299c89daa1598de8b06e3e19a8f52b7c555d8b92
whuchenrui <rayprogram@163.com>
2015-01-12 19:49:49 +0800

    增加test模块

--- application/web/urls.py
+++ application/web/urls.py
@@ -2,12 +2,12 @@
 __author__ = 'CRay'
 
 from handlers.PosBaisHandler import MainHandler
-from handlers.PosBaisHandler import PicHourHandler
 from handlers.RelatePicHandler import RelatePicHandler
+from handlers.TestHandler import TestHandler
 
 
 urls = [
     (r'/pb', MainHandler),
-    (r'/hour', PicHourHandler),
     (r'/page', RelatePicHandler),
+    (r'/test', TestHandler)
 ]
