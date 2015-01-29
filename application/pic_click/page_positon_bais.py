6c599860df5b440c68d291e56d5e1a295c789d2e
ruichen <rayprogram@163.com>
2015-01-29 22:10:28 +0800

    重构pb的计算函数

--- application/pic_click/page_positon_bais.py
+++ application/pic_click/page_positon_bais.py
@@ -1,76 +1,79 @@
 # coding=utf-8
 __author__ = 'CRay'
 
-import codecs
-from ConfigParser import ConfigParser
+import os
+from lib.Config import Config
+from lib import Function
 
 
-def position_bias(filter_req, _type):
-    cf = ConfigParser()
-    cf.read('..\\config\\data.conf')
-    source_path = cf.get('dataset', 'path')
-    action = int(cf.get('type', 'type'))
-
-    if _type == 'save':
-        name = 'result_raw'
+def position_bias(st_time, end_time, behavior):
+    """
+    :param behavior:  all 整个过滤后数据集   view 不包含save操作的集合  save 包含save操作的集合
+    :return:
+    dict_result    {1: [show_num, click_num]}  show_num: 当前页码一共展示多少次图片，click_num，一共被点击量
+    """
+    cf_data = Config('data.conf')
+    if 'all' == behavior:
+        fin_path = cf_data.get('path', 'filter_data')
+    elif 'view' == behavior:
+        fin_path = cf_data.get('path', 'view_data')
+    elif 'save' == behavior:
+        fin_path = cf_data.get('path', 'save_data')
     else:
-        name = 'result'
-    fin_result = open(source_path+name, 'r')
+        return False
+    chart_path = cf_data.get('path', 'chart_result')
 
-    dict_result = {}  # {1: [show_num, click_num]}  show_num: 当前页码一共展示多少次图片，click_num，一共被点击量
+    list_time = Function.get_time_list(st_time, end_time)
+    dict_result = {}
+    for day in list_time:
+        input_path = fin_path + day
+        if os.path.exists(input_path):
+            for i in range(0, 24):
+                temp_name = ''
+                if i < 10:
+                    temp_name = '0'
+                file_in = input_path + '\\full_' + temp_name + str(i)
+                if os.path.exists(file_in):
+                    fin = open(file_in, 'r')
+                    while True:
+                        line = fin.readline()
+                        if not line:
+                            break
+                        dict_line = eval(line)
+                        list_result = dict_line['result']
+                        list_pic = dict_line['pic']
+                        length = len(list_result)
+                        request_num = length/36
+                        for j in range(1, 5):
+                            if j not in dict_result:
+                                dict_result[j] = [0, 0]
+                            dict_result[j][0] += 9
+                            for k in range((j-1)*9, j*9):
+                                if list_result[k] >= 1:   # 这里统计点击情况
+                                    dict_result[j][1] += 1
+                        for j in range(2, request_num+1):
+                            page = j*4 - 3
+                            if page not in dict_result:
+                                dict_result[page] = [0, 0]
+                            dict_result[page][0] += 9
+                            for k in range((page-1)*9, page*9):
+                                if list_result[k] >= 1:
+                                    dict_result[page][1] += 1
+                    fin.close()
 
-    while True:
-        line_result = fin_result.readline()
-        if not line_result:
-            break
-        result = line_result.strip('\n').split(' ')
-        length = len(result)
-        requests = length/36
-        if requests > filter_req:
-            requests = filter_req
-        for index, item in enumerate(result):
-            result[index] = int(item)
-        for i in range(1, 5):           # 统计前四页的呈现点击情况
-            if i not in dict_result:
-                dict_result[i] = [0, 0]
-            dict_result[i][0] += 9
-            for j in range((i-1)*9, i*9):
-                if result[j] >= action:
-                    dict_result[i][1] += 1
-        for i in range(2, requests+1):
-            page = i*4 - 3
-            if page not in dict_result:
-                dict_result[page] = [0, 0]
-            dict_result[page][0] += 9
-            for j in range((page-1)*9, page*9):
-                if result[j] >= action:
-                    dict_result[page][1] += 1
-    fin_result.close()
-    print_result(dict_result)
-
-
-def print_result(result):
-    cf = ConfigParser()
-    cf.read('..\\config\\data.conf')
-    chart_data = cf.get('dataset', 'chart')
+    ## 把dict_result按照格式输出, 画成highcharts图
     a = []  # column
-    b = []  # probability per pic
-    for page in result:
-        a.append([page, result[page][0]])
-        prob = float(result[page][1])/result[page][0]/9
-        temp = round(prob, 4)
-        b.append([page, temp])
-    aa = []
-    bb = []
-    a.sort(key=lambda x: x[0])
-    for item in a:
-        aa.append(item[1])
-    b.sort(key=lambda x: x[0])
-    for item in b:
-        bb.append(item[1])
-    fout = open(chart_data+'4-position-bias.result', 'w')
+    b = []  # probability per page
+    page_num = len(dict_result)*4
+    for page in range(1, page_num):
+        if page in dict_result:
+            a.append([page, dict_result[page][0]])
+            prob = float(dict_result[page][1])/dict_result[page][0]
+            temp = round(prob, 4)
+            b.append([page, temp])
+    fout = open(chart_path+'4-position-bias-'+behavior+'.result', 'w')
     fout.write('column:\n')
-    fout.write(str(aa))
+    fout.write(str(a))
     fout.write('\n\nline:\n')
-    fout.write(str(bb))
+    fout.write(str(b))
     fout.close()
