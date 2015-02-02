9d7f28e911332184a17190e258695277dd6b4418
ruichen <rayprogram@163.com>
2015-02-02 16:46:04 +0800

    增加查看数据库名, 表名的方法

--- lib/Mongo.py
+++ lib/Mongo.py
@@ -10,11 +10,17 @@ class Mongo():
         self.conn = self.client[database]
         self.collection = self.conn[collection]
 
-    def conn_database(self, database):
+    def set_database(self, database):
         self.conn = self.client[database]
 
-    def conn_collection(self, collection):
+    def set_collection(self, collection):
         self.collection = self.conn[collection]
+
+    def get_current_database_name(self):
+        return self.conn.name
+
+    def get_current_collection_name(self):
+        return self.collection.name
 
     def close(self):
         self.client.close()
