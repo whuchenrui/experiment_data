e6af0c3f2354c21dea7a453c9c694d8107390b6d
ruichen <rayprogram@163.com>
2015-02-03 21:08:38 +0800

    计算相似度

--- application/evaluation/calculate_similarity.py
+++ application/evaluation/calculate_similarity.py
@@ -6,15 +6,17 @@ from lib import Function
 from lib.Mongo import Mongo
 
 
-# 引自:  Kendall's τ 《2002_Optimizing search engines using click through》
-def calculate_similarity(ranking_main, ranking_second):
+# 方法: Kendall's τ method.  引自: 《2002_Optimizing search engines using click through》
+def calculate_similarity(ranking_main, intersection):
+    """
+    计算两个序列的相似度, 方法可优化之处: 缩小 ranking_main大小, 或改用dict存储 (大量时间损耗在index方法上)
+    """
     concordant = 0
     discordant = 0
-    intersection = set(ranking_main) & set(ranking_second)
     for combination in itertools.combinations(intersection, 2):
         p1 = combination[0]
         p2 = combination[1]
-        result = (ranking_main.index(p1)-ranking_main.index(p2)) * (ranking_second.index(p1)-ranking_second.index(p2))
+        result = (ranking_main.index(p1)-ranking_main.index(p2)) * (intersection.index(p1)-intersection.index(p2))
         if result > 0:
             concordant += 1
         else:
@@ -27,10 +29,14 @@ def calculate_similarity(ranking_main, ranking_second):
         return 0
 
 
-def get_pic_group(st_time, end_time, st_page, end_page):
+def get_pic_group(st_time, end_time, page_num):
+    """
+    获得时间范围内每个页码上出现的图片集合
+    dict_page_pic_group: {page: (pic1, pic2 ...)}
+    """
     list_time = Function.get_time_list(st_time, end_time)
     dict_page_pic_group = {}
-    for page in range(st_page, end_page+1):
+    for page in range(1, page_num+1):
         dict_page_pic_group[page] = set()
     mongo = Mongo('kdd', 'hour_ranking')
     for day in list_time:
@@ -43,21 +49,27 @@ def get_pic_group(st_time, end_time, st_page, end_page):
             record = mongo.collection.find({'time': rank_time}, {'_id': 0})
             if record.count() > 0:
                 list_result = record[0]['ranking']
-                for page in range(st_page, end_page+1):
+                for page in range(1, page_num+1):
                     selected_pic = list_result[(page-1)*9: page*9]
                     for pic in selected_pic:
                         dict_page_pic_group[page].add(pic)
             else:
                 print rank_time, 'is missing!'
+    mongo.close()
+    return dict_page_pic_group
 
+
+def get_test_data_ranking(st_time, end_time, pic_group, target_ranking):
     # 计算图片的点击概率
+    list_time = Function.get_time_list(st_time, end_time)
+    mongo = Mongo('kdd', 'hour_ranking')
     dict_pic_click_info = {}
     mongo.set_collection('pic_click_info')
-    set_target_ranking = set(Target_ranking)
-    for page in dict_page_pic_group:
+    set_target_ranking = set(target_ranking)
+    for page in pic_group:
         if page not in dict_pic_click_info:
             dict_pic_click_info[page] = {}
-        intersection = set_target_ranking & dict_page_pic_group[page]
+        intersection = set_target_ranking & pic_group[page]
         for pic in intersection:
             if pic not in dict_pic_click_info[page]:
                 dict_pic_click_info[page][pic] = [0, 0]
@@ -84,12 +96,13 @@ def get_pic_group(st_time, end_time, st_page, end_page):
                 prob = float(dict_pic_click_info[page][pic][1])/dict_pic_click_info[page][pic][0]
                 temp_rank.append([round(prob, 4), pic])
             else:
-                print page, pic
+                # 数据库中数据过滤了呈现<50的记录,所以该图片如果几天内没有出现在这一页,那么这一页上的呈现就为0
+                print page, pic, dict_pic_click_info[page][pic]
         temp_rank.sort(key=lambda x: x[0], reverse=True)
         for item in temp_rank:
             list_pic.append(item[1])
         dict_result[page] = list_pic
-    return dict_page_pic_group, dict_pic_click_info, dict_result
+    return dict_pic_click_info, dict_result
 
 
 def draw_chart(compare_ranking, input_dict):
@@ -98,16 +111,15 @@ def draw_chart(compare_ranking, input_dict):
         second_ranking = input_dict[page]
         similarity = calculate_similarity(compare_ranking, second_ranking)
         result.append([page, similarity])
-        print str(page), '  ', compare_ranking, second_ranking
+        # print str(page), '  ', compare_ranking, second_ranking
     print result
+    return result
 
 
 if __name__ == '__main__':
-    Target_ranking = ['206513153', '201058305', '184364289', '179245825', '203859969', '210742529', '213793025']
-    St_time = '2014-11-04'
-    End_time = '2014-11-04'
-    St_page = 1
-    End_page = 4
-    Pic_group, Pic_click_info, Pic_page_ranking = get_pic_group(St_time, End_time, St_page, End_page)
-    draw_chart(Target_ranking, Pic_page_ranking)
+    St_time = '2014-11-07'
+    End_time = '2014-11-10'
+    Page_num = 30
+    # Pic_group, Pic_click_info, Pic_page_ranking = get_pic_group(St_time, End_time, Page_num, Target_ranking)
+    # draw_chart(Target_ranking, Pic_page_ranking)
     # print Pic_group
