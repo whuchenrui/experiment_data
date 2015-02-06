7c043f90c0652f69fa3b744562641cc1ec88494d
ruichen <rayprogram@163.com>
2015-02-06 20:06:20 +0800

    去除时间统计

--- application/evaluation/draw_evaluation.py
+++ application/evaluation/draw_evaluation.py
@@ -5,28 +5,18 @@ __author__ = 'CRay'
 """
 import calculate_similarity
 import make_pic_collection
-from datetime import datetime
 from lib.Config import Config
 
 
 def draw_evaluation(train_st, train_end, pic_num, test_st, test_end, page_num):
-    start = datetime.now()
     test_group = calculate_similarity.get_pic_group(test_st, test_end, page_num)
-    now = datetime.now()
-    print 'test group 构造耗时: ', now - start
     data3_full_ranking_raw, data_name = make_pic_collection.data_full_model(pic_num)
     data2_pb_ranking, data3_full_ranking, pic_num = make_pic_collection.data_position_bias(data3_full_ranking_raw)
-    now2 = datetime.now()
-    print '3个Ranking 构造耗时: ', now2 - now
 
     # 画 full model 的 similarity 数据
     test_info, test_ranking = \
         calculate_similarity.get_test_data_ranking(test_st, test_end, test_group, data3_full_ranking)
-    now3 = datetime.now()
-    print '计算出test天数上每页上的图片的使用信息耗时: ', now3 - now2
     similarity_full = calculate_similarity.draw_chart(data3_full_ranking, test_ranking)
-    now_full = datetime.now()
-    print 'full 用时: ', now_full - now3
     # end
 
     # 画 position bias 的 similarity 数据
@@ -34,8 +24,6 @@ def draw_evaluation(train_st, train_end, pic_num, test_st, test_end, page_num):
         calculate_similarity.get_test_data_ranking(test_st, test_end, test_group, data2_pb_ranking)
     similarity_pb = calculate_similarity.draw_chart(data2_pb_ranking, test_ranking)
     # end
-    now_pb = datetime.now()
-    print 'pb 用时: ', now_pb - now_full
 
     # 画 baseline 的 similarity 数据
     # 按照 click 逆序后 算相似度
@@ -44,20 +32,16 @@ def draw_evaluation(train_st, train_end, pic_num, test_st, test_end, page_num):
     test_info, test_ranking = \
         calculate_similarity.get_test_data_ranking(test_st, test_end, test_group, data1_click_ranking)
     similarity_click = calculate_similarity.draw_chart(data1_click_ranking, test_ranking)
-    now_click = datetime.now()
-    print 'click 用时: ', now_click - now_pb
 
     # 按照prob 逆序后 算相似度
     test_info, test_ranking = \
         calculate_similarity.get_test_data_ranking(test_st, test_end, test_group, data1_prob_ranking)
     similarity_prob = calculate_similarity.draw_chart(data1_prob_ranking, test_ranking)
-    now_prob = datetime.now()
-    print 'prob 用时: ', now_prob-now_click
 
     cf_data = Config('data.conf')
     data_path = cf_data.get('path', 'dataset_path')
     chart_path = cf_data.get('path', 'chart_result')
-    file_name = 'train_'+train_st+'_'+train_end+'_K='+str(pic_num)+'_test_'+test_st+'_'+test_end+'_L='+str(page_num)+'_filter-'+data_name
+    file_name = 'train_'+train_st+'_'+train_end+'_K='+str(pic_num)+'_test_'+test_st+'_'+test_end+'_L='+str(page_num)+data_name
     fout_data = open(data_path+file_name+'.data', 'w')
     fout_data.write('Baseline click data: \n' + str(similarity_click) + '\n\n')
     fout_data.write('Baseline prob data: \n' + str(similarity_prob) + '\n\n')
