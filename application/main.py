29f1a760bcaf86a76aac23c0f1907231e390b3c2
ruichen <rayprogram@163.com>
2015-02-09 14:55:53 +0800

    整洁...

--- application/main.py
+++ application/main.py
@@ -5,7 +5,6 @@ from application.raw_data import filter_dataset, split_dataset
 from application.draw_charts import page_positon_bais, turn_probability, basic_data, picture_quality
 from application.evaluation import init_db, draw_evaluation
 from datetime import datetime
-from lib.Config import Config
 
 
 def exec_dataset(st_time, end_time):
@@ -19,14 +18,14 @@ def exec_dataset(st_time, end_time):
         print traceback.format_exc()
 
 
-def draw_chart(st_time, end_time, behavior):
+def draw_chart(st_time, end_time, behavior, max_req):
     try:
         picture_quality.quality(st_time, end_time, 500)
-        # basic_data.basic_data(st_time, end_time)
-        # page_positon_bais.position_bias(st_time, end_time, behavior)         # page的postion图
-        # print 'Page position bias finish !'
-        # turn_probability.turn_probability(st_time, end_time, behavior, 3, 15, 2, 0, 20)
-        # print 'Turn probability with last 8 pages finish !'
+        basic_data.basic_data(st_time, end_time)
+        page_positon_bais.position_bias(st_time, end_time, behavior, max_req)         # page的postion图
+        print 'Page position bias finish !'
+        turn_probability.turn_probability(st_time, end_time, behavior, 8, 60, 8, 0, 20)
+        print 'Turn probability with last 8 pages finish !'
     except:
         print traceback.format_exc()
 
@@ -37,12 +36,14 @@ def exec_evaluation(train_st, train_end, pic_num, test_st, test_end, page_num):
 
 if __name__ == '__main__':
     Select_dataset = 'all'  # view 对应 view数据集,  save对应save数据集   all 对应所有数据集
-    Train_st_time = '2014-11-07'
+    Train_st_time = '2014-11-04'
     Train_end_time = '2014-11-11'
     K = 1500  # 图片集合个数
     Test_st_time = '2014-11-12'
-    Test_end_time = '2014-11-16'
+    Test_end_time = '2014-11-18'
     L = 40  # 展现的页码
-    for pic_number in [2000]:
+    for pic_number in [500, 1200, 1900, 2600]:
+        now = datetime.now()
         exec_evaluation(Train_st_time, Train_end_time, pic_number, Test_st_time, Test_end_time, L)
+        print 'K='+str(pic_number)+' 用时: ', datetime.now()-now
     # draw_chart(Train_st_time, Train_end_time, Select_dataset)
