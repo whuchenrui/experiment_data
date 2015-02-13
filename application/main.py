7999bdb416c47a54f0643824df9933477a4b4219
ruichen <rayprogram@163.com>
2015-02-13 22:58:24 +0800

    区分是否有save序列

--- application/main.py
+++ application/main.py
@@ -30,20 +30,25 @@ def draw_chart(st_time, end_time, behavior, max_req):
         print traceback.format_exc()
 
 
-def exec_evaluation(train_st, train_end, pic_num, test_st, test_end, page_num):
-    draw_evaluation.draw_evaluation(train_st, train_end, pic_num, test_st, test_end, page_num)
+def exec_evaluation(behavior, train_st, train_end, pic_num, test_st, test_end, page_num, data2, data3):
+    draw_evaluation.draw_evaluation(behavior, train_st, train_end, pic_num, test_st, test_end, page_num, data2, data3)
 
 
 if __name__ == '__main__':
-    Select_dataset = 'all'  # view 对应 view数据集,  save对应save数据集   all 对应所有数据集
     Train_st_time = '2014-11-04'
-    Train_end_time = '2014-11-11'
-    K = 1500  # 图片集合个数
-    Test_st_time = '2014-11-12'
-    Test_end_time = '2014-11-18'
-    L = 40  # 展现的页码
-    for pic_number in [500, 1200, 1900, 2600]:
+    Train_end_time = '2014-11-05'
+
+    Test_st_time = '2014-11-04'
+    Test_end_time = '2014-11-05'
+
+    data2_name = '1107-1111_data2_pb'
+    data3_name = '1107-1111_data3_full_initial_para_window_8_extra_end_iter10'
+    L = 40      # 展现的页码
+    Union_size = [800]
+    Behavior = 'click'   # 3个值, save(所有序列都含有2), click(所有序列都没有2), all(前面2个序列汇总)
+                         # behavior 只印象train集合中, 按照点击数量和点击概率排序的ranking, 不作用于test集合
+    for pic_number in Union_size:
         now = datetime.now()
-        exec_evaluation(Train_st_time, Train_end_time, pic_number, Test_st_time, Test_end_time, L)
+        exec_evaluation(Behavior, Train_st_time, Train_end_time, pic_number, Test_st_time, Test_end_time, L, data2_name, data3_name)
         print 'K='+str(pic_number)+' 用时: ', datetime.now()-now
-    # draw_chart(Train_st_time, Train_end_time, Select_dataset)
+    draw_evaluation.draw_ndcg(Behavior, data3_name)
